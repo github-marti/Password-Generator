5594d350672a4629a73add3a7fc654b020f33927
Marti <52050778+github-marti@users.noreply.github.com>
2019-09-18 17:26:49 -0700

    changed index.js so that prompts are run after clicking the generate password button. made aesthetic changes to style.css

--- index.js
+++ index.js
@@ -57,61 +57,6 @@ let lowerCaseAlphabet = lowerCase.split('');
 let upperCaseAlphabet = upperCase.split('');
 
 
-// create empty arrays for accepting user character choices and making the new password
-
-let charArray = [];
-
-let newPassword = [];
-
-
-// create prompt to ask user for password length and then assign it to a variable
-
-let passwordLength = prompt("How many characters do you want your password to be? Choose a number between 8 and 128.");
-
-
-// return repeat prompt if password is not between 8 and 128 characters lomg
-
-while (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("Invalid password length. Please choose a number between 8 and 128.");
-}
-
-
-// ask user which character sets they want included in their password
-
-let specialCharConfirm = confirm("Do you want your password to contain special characters?");
-let numericCharConfirm = confirm("Do you want your password to contain numeric characters?");
-let lowerCaseConfirm = confirm("Do you want your password to contain lower case characters?");
-let upperCaseConfirm = confirm("Do you want your password to contain upper case characters?");
-
-if (specialCharConfirm) {
-    charArray.push(specialChar);
-}
-
-if (numericCharConfirm) {
-    charArray.push(numericChar);
-}
-
-if (lowerCaseConfirm) {
-    charArray.push(lowerCaseAlphabet);
-}
-
-if (upperCaseConfirm) {
-    charArray.push(upperCaseAlphabet);
-}
-
-
-// return an error message if user did not choose at least one character set
-
-if (!specialCharConfirm && !numericCharConfirm && !lowerCaseConfirm && !upperCaseConfirm) {
-    alert("You must choose at least one character set. Please refresh page and start again.")
-}
-
-
-// make sure arrays are pushing to charArray
-
-console.log(charArray);
-
-
 // functions for selecting an array from the user's selected character groups
 // and for adding a new character to the password array
 
@@ -126,15 +71,6 @@ let addCharacter = (array) => {
 }
 
 
-// function that writes new password to the textarea
-
-let printPassword = () => {
-    let password = newPassword.join('');
-    document.getElementById("password").innerHTML = password;
-    document.getElementById("copy").disabled = false;
-}
-
-
 // function that copies password to clipboard
 
 let copyPassword = () => {
@@ -146,13 +82,81 @@ let copyPassword = () => {
 }
 
 
-// for loop which adds as many characters to the password as specified by user
+// function creates random password based on user input and then prints it to textarea
 
-for (i = 0; i < passwordLength; i++) {
-    addCharacter(arraySelect());
+let printPassword = () => {
+
+    // create empty arrays for accepting user character choices and making the new password
+
+    charArray = [];
+    newPassword = [];
+
+
+    // create prompt to ask user for password length and then assign it to a variable
+
+    let passwordLength = prompt("How many characters do you want your password to be? Choose a number between 8 and 128.");
+
+
+    // show prompt again if password is not between 8 and 128 characters lomg
+
+    while (passwordLength < 8 || passwordLength > 128) {
+        passwordLength = prompt("Invalid password length. Please choose a number between 8 and 128.");
+    }
+
+
+    // ask user which character sets they want included in their password then push them to the array
+
+    let specialCharConfirm = confirm("Do you want your password to contain special characters?");
+    let numericCharConfirm = confirm("Do you want your password to contain numeric characters?");
+    let lowerCaseConfirm = confirm("Do you want your password to contain lower case characters?");
+    let upperCaseConfirm = confirm("Do you want your password to contain upper case characters?");
+
+    if (specialCharConfirm) {
+        charArray.push(specialChar);
+    }
+
+    if (numericCharConfirm) {
+        charArray.push(numericChar);
+    }
+
+    if (lowerCaseConfirm) {
+        charArray.push(lowerCaseAlphabet);
+    }
+
+    if (upperCaseConfirm) {
+        charArray.push(upperCaseAlphabet);
+    }
+
+
+    // return an error message if user did not choose at least one character set
+
+    if (!specialCharConfirm && !numericCharConfirm && !lowerCaseConfirm && !upperCaseConfirm) {
+        alert("You must choose at least one character set. Please try again.")
+    }
+
+
+    // make sure arrays are pushing to charArray
+
+    console.log(charArray);
+
+    
+    // select random array and then select random character from that array to create
+    // password of the length chosen by user
+
+    for (i = 0; i < passwordLength; i++) {
+        addCharacter(arraySelect());
+    }
+
+
+    // join password array into a string and print it into the textarea
+
+    let password = newPassword.join('');
+    document.getElementById("password").value = password;
+
+
+    // make the copy button active
+
+    document.getElementById("copy").disabled = false;
 }
 
 
-// check to make sure for loop is creating a random password array
-
-console.log(newPassword)
